a47f63bba4def46d4420867f8646e13df865662e
HungTruong4689 <hungphuc4689@gmail.com>
2023-12-25 19:20:42 +0200

    update setting page

--- src/pages/SettingPage.tsx
+++ src/pages/SettingPage.tsx
@@ -5,12 +5,11 @@ import { addChart, updateChart, removeChart, ChartInstance } from '../redux/char
 import ChartConfigModal from '../components/ChartConfigModal'
 import UIChart from '../components/UIChart'
 
-interface SettingPageProps {
-    // Define your props here
-}
+interface SettingPageProps {}
 
 const SettingPage: React.FC<SettingPageProps> = () => {
     const [isModalOpen, setModalOpen] = useState(false)
+    const [isAddChart, setIsAddChart] = useState('')
     const [currentChart, setCurrentChart] = useState<ChartInstance | undefined>(undefined)
     const charts = useSelector((state: RootState) => state.chartConfig.charts)
     const dispatch = useDispatch()
@@ -18,11 +17,13 @@ const SettingPage: React.FC<SettingPageProps> = () => {
     const handleAddChart = () => {
         setCurrentChart(undefined)
         setModalOpen(true)
+        setIsAddChart('add')
     }
 
     const handleUpdateChart = (chart: ChartInstance) => {
         setCurrentChart(chart)
         setModalOpen(true)
+        setIsAddChart('update')
     }
 
     const handleDeleteChart = (chartId: number) => {
@@ -38,30 +39,16 @@ const SettingPage: React.FC<SettingPageProps> = () => {
         setModalOpen(false)
     }
 
-    // const generateRandomData = () => {
-    //     const data = []
-    //     let currentValue = 0
-
-    //     while (currentValue <= 20) {
-    //         const value = Math.floor(Math.random() * 100) // Generate random values between 0 and 100
-    //         data.push(value)
-
-    //         currentValue++
-    //     }
-
-    //     return data
-    // }
-
-    // const randomData = generateRandomData()
-
     return (
         <div className='container mx-auto p-4'>
-            <button
-                onClick={handleAddChart}
-                className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4'
-            >
-                Add New Chart
-            </button>
+            <div className='flex justify-center'>
+                <button
+                    onClick={handleAddChart}
+                    className='bg-gradient-to-r from-teal-500 to-blue-500 hover:from-teal-600 hover:to-blue-600 text-white font-bold py-2 px-4 rounded-full transition duration-300 mb-4'
+                >
+                    Add New Chart
+                </button>
+            </div>
 
             {isModalOpen && (
                 <ChartConfigModal
@@ -69,6 +56,7 @@ const SettingPage: React.FC<SettingPageProps> = () => {
                     onClose={() => setModalOpen(false)}
                     onSave={handleSaveChart}
                     isUpdate={currentChart !== null}
+                    isAddChart={isAddChart}
                 />
             )}
 
@@ -77,20 +65,21 @@ const SettingPage: React.FC<SettingPageProps> = () => {
                     <li key={chart.id} className='bg-white rounded-lg overflow-hidden shadow-md'>
                         <UIChart type={chart.type} color={chart.color} />
 
-                        <div className='p-4'>
-                            <h3 className='text-lg font-semibold mb-2'>
-                                {chart.name} - {chart.type}
-                            </h3>
+                        <div className='p-4 bg-gray-100 rounded-lg shadow-md'>
+                            <div className='mb-4'>
+                                <h3 className='text-2xl font-bold text-indigo-700'>{chart.name}</h3>
+                                <p className='text-sm text-gray-600'>{chart.type}</p>
+                            </div>
                             <div className='flex space-x-2'>
                                 <button
                                     onClick={() => handleUpdateChart(chart)}
-                                    className='bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-1 px-3 rounded flex-1'
+                                    className='bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded-full flex-1'
                                 >
                                     Update
                                 </button>
                                 <button
                                     onClick={() => handleDeleteChart(chart.id)}
-                                    className='bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-3 rounded flex-1'
+                                    className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full flex-1'
                                 >
                                     Delete
                                 </button>
